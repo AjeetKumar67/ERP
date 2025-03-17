6d9b18ffcad5031dd66feacd45378de3e1b2114f
AjeetKumar67 <ajeetyadav6716@gmail.com>
2025-03-17 20:43:46 +0530

    Git_Test

--- management/User/urls.py
+++ management/User/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import RegisterView, OTPVerificationView, LoginView, LogoutView, EditProfileView, ResetPasswordView, ChangePasswordView
-
+# ghfjg
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('verify-otp/', OTPVerificationView.as_view(), name='verify-otp'),
